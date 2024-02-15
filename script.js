beb63e8436b532e8b8b93a6ac92d515f7949d006
Sunny <155150793+sandeep-310@users.noreply.github.com>
2024-02-15 13:14:35 +0530

    Login page

---
+++ script.js
@@ -0,0 +1,13 @@
+let button = document.querySelectorAll("button")
+let buttonArray = Array.from(button);
+
+buttonArray.forEach(element => {
+    element.addEventListener("click",(e)=>{
+        document.getElementsByClassName("loginDiv")[0].classList.toggle("toggleClass");
+        document.getElementsByClassName("SignupDiv")[0].classList.toggle("toggleClass");
+        document.getElementsByClassName("subLoginDiv")[0].classList.toggle("magicAnimation")
+        document.getElementsByClassName("welcome-back")[0].classList.toggle("toggleClass");
+        document.getElementsByClassName("welcome")[0].classList.toggle("toggleClass");
+        console.log(e);
+    })
+});
